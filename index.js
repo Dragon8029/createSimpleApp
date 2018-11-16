be7c5d9cab386de2d24e11f3c19da38387d17db5
Cam Utz <Dragon8029@gmail.com>
2018-11-16 06:44:32 -0700

    add chat.js file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-//socket.io instantiation
-const io = require("socket.io")(server);
 
 //set the template engine ejs
 app.set('view engine', 'ejs');
@@ -13,9 +11,14 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+//Listen on port 3000
+server = app.listen(3000);
+
+//socket.io instantiation
+const io = require("socket.io")(server);
+
 //listen on every connection
 io.on('connection', (socket) => {
     console.log('New user connected');
 });
-//Listen on port 3000
-server = app.listen(3000);
