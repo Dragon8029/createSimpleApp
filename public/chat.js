729f560bec42185a52933fa1081b9627c32d92a9
Cam Utz <Dragon8029@gmail.com>
2018-11-16 07:00:18 -0700

    some changes to chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,19 @@
 $(function(){
     //make connection
-    var socket = io.connect('http://localhost:3000');
+    var socket = io.connect('http://localhost:3000')
+
+    //buttons and inputs
+    var message = $("#message")
+    var username = $("#username")
+    var send_message = $("#send_message")
+    var send_username = $("#send_username")
+    var chatroom = $("#chatroom")
+
+    
+
+    //Emit a username
+    send_username.click(function(){
+        console.log(username.val())
+        socket.emit('change_username', {username : username.val()})
+    })
 });
